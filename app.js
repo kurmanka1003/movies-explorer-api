c809b44c165b805ef5d09a61a24c0642367d3a13
Rumiya Kurmangazieva <kurmangazieva_2001@mail.ru>
2023-08-25 17:49:13 +0300

    исправление ошибки корс

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const cors = require('./middlewares/cors');
 const errors = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -18,7 +18,11 @@ const app = express();
 
 app.use(requestLogger);
 
-app.use(cors);
+app.use(cors({
+  origin: 'https://filmfinder.nomoredomainsicu.ru',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());
 
