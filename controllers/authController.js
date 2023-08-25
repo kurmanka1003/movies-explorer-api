41c4a3333c93398e4137551f89b43aee3f9e46b5
Rumiya Kurmangazieva <kurmangazieva_2001@mail.ru>
2023-08-25 22:39:39 +0300

    исправление контроллера

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,65 +5,63 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
-const BadRequestError = require('../utils/errors/badRequestError');
-const UnauthorizedError = require('../utils/errors/unauthorizedError');
-const ConflictError = require('../utils/errors/conflictError');
+const { responseMessage } = require('../utils/config');
 
-const { CREATED_STATUS } = require('../utils/constants');
+const { CREATED_STATUS, SUCCESS_STATUS } = require('../utils/constants');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 // POST /signup
-const signup = async (req, res, next) => {
-  try {
-    const { email, password, name } = req.body;
+const signup = (req, res, next) => {
+  const {
+    email,
+    password,
+    name,
+  } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return next(new ConflictError('Пользователь с таким e-mail уже существует'));
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create({ email, password: hashedPassword, name });
-
-    return res.status(CREATED_STATUS).json({ message: 'Пользователь успешно зарегистрирован' });
-  } catch (err) {
-    if (err instanceof Error.ValidationError) {
-      return next(new BadRequestError('Переданы некорректные данные при создании пользователя.'));
-    }
-    return next(err);
-  }
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email,
+      password: hash,
+      name,
+    }))
+    .then((user) => {
+      responseMessage(res, CREATED_STATUS, {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+    })
+    .catch(next);
 };
 
 // POST /signin
-const signin = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email }).select('+password').orFail();
-    const match = await bcrypt.compare(password, user.password);
-    if (match) {
+const signin = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'rumiya-secret-key', {
         expiresIn: '7d',
       });
-      res.cookie('jwtToken', token, {
+
+      res.cookie('jwt', token, {
         maxAge: 3600,
         httpOnly: true,
-      });
-      return res.send({ jwtToken: token });
-    }
-    throw new UnauthorizedError('Передан неверный e-mail или пароль');
-  } catch (err) {
-    if (err instanceof Error.DocumentNotFoundError) {
-      return next(new UnauthorizedError('Передан неверный e-mail или пароль'));
-    }
-    return next(err);
-  }
+      })
+        .send({ message: 'Авторизация прошла успешно' });
+    })
+    .catch(next);
 };
 
 // POST /signout
 const signout = (req, res) => {
-  res.clearCookie('jwtToken');
-  return res.json({ message: 'Вы успешно вышли' });
+  try {
+    res.clearCookie('jwt');
+    return responseMessage(res, SUCCESS_STATUS, { message: 'Вы успешно вышли' });
+  } catch (err) {
+    return new Error('Произошла ошибка при попытке выйти из акканута');
+  }
 };
 
 module.exports = { signup, signin, signout };
