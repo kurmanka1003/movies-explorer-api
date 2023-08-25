33ba757bcdcd729208aa3032bbfa1c7ca88f0c93
Rumiya Kurmangazieva <kurmangazieva_2001@mail.ru>
2023-08-25 21:46:30 +0300

    исправление контроллера

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,50 +1,25 @@
-const { ValidationError } = require('mongoose').Error;
-
 const User = require('../models/user');
-
-const BadRequestError = require('../utils/errors/badRequestError');
-const NotFoundError = require('../utils/errors/notFoundError');
+const { responseMessage } = require('../utils/config');
 
 const {
   SUCCESS_STATUS,
 } = require('../utils/constants');
 
-const formatUserData = (user) => ({
-  name: user.name,
-  _id: user._id,
-  email: user.email,
-});
-
 // GET /users/me
-
-const getCurrentUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.user._id).orFail(() => {
-      throw new NotFoundError('Указанный идентификатор пользователя не найден');
-    });
-    res.status(SUCCESS_STATUS).send(formatUserData(user));
-  } catch (err) {
-    next(err);
-  }
+const getCurrentUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => responseMessage(res, SUCCESS_STATUS, { data: user }))
+    .catch(next);
 };
 
 // PATCH /users/me
-const updateUser = async (req, res, next) => {
-  try {
-    const updateData = req.body;
-    const user = await User.findByIdAndUpdate(req.user._id, updateData, {
-      new: true,
-      runValidators: true,
-    });
+const updateUser = (req, res, next) => {
+  const owner = req.user._id;
+  const { email, name } = req.body;
 
-    return res.status(SUCCESS_STATUS).send(formatUserData(user));
-  } catch (err) {
-    if (err instanceof ValidationError) {
-      return next(new BadRequestError('Переданы некорректные данные при обновлении пользователя.'));
-    }
-
-    return next(err);
-  }
+  User.findByIdAndUpdate(owner, { email, name }, { new: true, runValidators: true })
+    .then((userInfo) => responseMessage(res, SUCCESS_STATUS, { data: userInfo }))
+    .catch(next);
 };
 
 module.exports = { getCurrentUser, updateUser };
